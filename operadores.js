21ac60bdcfcb699ae50bc8049f0103c860c24c46
Victor Vaz <134503953+vaxzzzz@users.noreply.github.com>
2023-08-22 09:16:12 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,36 @@
+function soma(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) + parseInt(val2);
+    alert(result);                                    
+    
+
+
+}
+function menos(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) - parseInt(val2);
+    alert(result);
+    
+
+
+}
+function vezes(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) * parseInt(val2);
+    alert(result);
+    
+
+
+}
+function divisão(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) / parseInt(val2);
+    alert(result);
+    
+
+
+}
